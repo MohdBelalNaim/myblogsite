d34efbd09312fec21f74dbc0b69834358a7ac0ee
Belal <contactbelalnaim@gmail.com>
2023-08-07 11:55:38 +0530

    Added JWT and login logic

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const app = express()
 const port = process.env.PORT
 const User = require('./models/user')
+const jwt = require('jsonwebtoken')
 
 mongoose.connect(process.env.MONGO_URI)
 .then(connected=>console.log("Connected to mongoDB"))
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.use(express.json())
 
 app.post("/signup",(req,res)=>{
+    
     const {name,email,password} = req.body
     
     if(!name || !email || !password){
@@ -22,7 +24,7 @@ app.post("/signup",(req,res)=>{
         .then(found=>{
             
             if(found){
-                res.json({error:"This email is already in use"})
+               res.json(found)
             }
 
             else{
@@ -42,8 +44,51 @@ app.post("/signup",(req,res)=>{
     
 })
 
+app.post("/login",(req,res)=>{
+    const {email,password} = req.body
+    if(!email || !password){
+        res.json({error:"All fields are required"})
+    }
+    else{
+
+        User.findOne({email:email})
+        .then(found=>{
+            if(found){
+                if(found.password==password){
+                    const token = jwt.sign({email:found.email},process.env.JWT_SECRET)
+                    res.json({token:token})
+                }
+                else{
+                    res.json({error:"Invalid credentials"})
+                }
+            }
+            else{
+                res.json({error:"This email is not registered"})
+            }
+        })
+
+    }
+})
+
+//token is generated while logging in
+//-> To validate the user
+//-> To manipulate th content using authorized methods
 
 
+
+//post data, post title, user who posted 
+// post data, post title -> provided by user
+// user who posted -> token
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
 
 
 app.listen(port,()=>console.log("App is running on port ",port))
